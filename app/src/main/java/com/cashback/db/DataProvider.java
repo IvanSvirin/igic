9fb7a4f2e124ad1a5bb6bfae9ca6619821feb3f0
IvanSvirin <ivan_svirin@mail.ru>
2016-04-16 18:13:44 +0600

    db working

--- app/src/main/java/com/cashback/db/DataProvider.java
+++ app/src/main/java/com/cashback/db/DataProvider.java
@@ -1,14 +1,18 @@
 package com.cashback.db;
 
 import android.content.ContentProvider;
+import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
+import android.util.Log;
 
 /**
  * Created by ivansv on 16.04.2016.
@@ -54,25 +58,94 @@ public class DataProvider extends ContentProvider {
                     sortOrder = DataContract.Merchants.COLUMN_NAME + " COLLATE NOCASE ASC";
                 cursor = db.query(DataContract.Merchants.TABLE_NAME, projection, selection, null, null, null, sortOrder);
                 return cursor;
+            case MERCHANT_BY_ID:
+                common_selection = DataContract.Merchants.COLUMN_VENDOR_ID + " = " + uri.getLastPathSegment();
+                if (!TextUtils.isEmpty(selection))
+                    common_selection = selection + " AND " + common_selection;
+                cursor = db.query(DataContract.Merchants.TABLE_NAME, projection, common_selection, null, null, null, null);
+                break;
+            case MERCHANTS_BY_IDS:
+                if (TextUtils.isEmpty(sortOrder)) {
+                    sortOrder = DataContract.Merchants.COLUMN_NAME;
+                }
+                common_selection = DataContract.Merchants.COLUMN_VENDOR_ID + " IN (" + uri.getLastPathSegment() + ")";
+                if (!TextUtils.isEmpty(selection)) {
+                    common_selection = selection + " AND " + common_selection;
+                }
+                cursor = db.query(true, DataContract.Merchants.TABLE_NAME, projection, common_selection, null, null, null, sortOrder, null);
+                break;
+
             default:
                 throw new UnsupportedOperationException("Unknown uri: " + uri);
         }
+        return cursor;
     }
 
     @Nullable
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        return null;
+        final SQLiteDatabase db = dbHelper.getWritableDatabase();
+        final int match = uriMatcher.match(uri);
+        ContentResolver resolver = context.getContentResolver();
+        long rowID;
+        Uri resultUri = null;
+        switch (match) {
+            case MERCHANTS:
+                rowID = db.insertWithOnConflict(DataContract.Merchants.TABLE_NAME, null, values, SQLiteDatabase.CONFLICT_REPLACE);
+                if (rowID > 0) {
+                    resultUri = ContentUris.withAppendedId(DataContract.URI_MERCHANTS, rowID);
+                } else {
+                    throw new SQLException("Failed to insert row into " + uri);
+                }
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        resolver.notifyChange(resultUri, null);
+        return resultUri;
     }
 
     @Override
     public int bulkInsert(Uri uri, ContentValues[] values) {
-        return super.bulkInsert(uri, values);
+        final SQLiteDatabase db = dbHelper.getWritableDatabase();
+        final int match = uriMatcher.match(uri);
+        int countInsert = 0;
+        switch (match) {
+            case MERCHANTS:
+                db.beginTransaction();
+                try {
+                    delete(uri, null, null);
+                    for (ContentValues val : values) {
+                        insert(uri, val);
+                        countInsert++;
+                    }
+                    db.setTransactionSuccessful();
+                } catch (SQLException e) {
+                    Log.e(LOG_TAG, e.getMessage());
+                } finally {
+                    db.endTransaction();
+                }
+                break;
+        }
+        context.getContentResolver().notifyChange(uri, null);
+        return countInsert;
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
+        final SQLiteDatabase db = dbHelper.getWritableDatabase();
+        final int match = uriMatcher.match(uri);
+        String id;
+        int affectedRowsCount;
+        switch (match) {
+            case MERCHANTS:
+                affectedRowsCount = db.delete(DataContract.Merchants.TABLE_NAME, null, null);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        context.getContentResolver().notifyChange(uri, null);
+        return affectedRowsCount;
     }
 
     @Override
@@ -83,6 +156,17 @@ public class DataProvider extends ContentProvider {
     @Nullable
     @Override
     public String getType(Uri uri) {
-        return null;
+        final int match = uriMatcher.match(uri);
+        switch (match) {
+            case MERCHANTS:
+                return DataContract.Merchants.CONTENT_TYPE;
+            case MERCHANT_BY_ID:
+                return DataContract.Merchants.CONTENT_ITEM_TYPE;
+            case MERCHANTS_BY_IDS:
+                return DataContract.Merchants.CONTENT_ITEM_TYPE;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+
+        }
     }
 }
