395340508b810e151c18737cee34e9a9cf7a723d
I.Svirin <Ivan.Svirin@softomate.com>
2016-05-24 11:37:23 +0600

    AllStoresFragment

--- app/src/main/java/com/cashback/db/DataContract.java
+++ app/src/main/java/com/cashback/db/DataContract.java
@@ -42,6 +42,24 @@ public class DataContract {
                 ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + "merchants";
     }
 
+    public static class Merchants implements BaseColumns {
+        public static final String TABLE_NAME = "merchants";
+        public static final String COLUMN_VENDOR_ID = "vendor_id";
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_COMMISSION = "commission";
+        public static final String COLUMN_EXCEPTION_INFO = "exception_info";
+        public static final String COLUMN_DESCRIPTION = "description";
+        public static final String COLUMN_GIFT_CARD = "gift_card";
+        public static final String COLUMN_AFFILIATE_URL = "affiliate_url";
+        public static final String COLUMN_LOGO_URL = "logo_url";
+        public static final String COLUMN_IS_FAVORITE = "is_favorite";
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + "merchants";
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + "merchants";
+    }
+
     public static class Coupons implements BaseColumns {
         public static final String TABLE_NAME = "coupons";
         public static final String COLUMN_COUPON_ID = "coupon_id";
