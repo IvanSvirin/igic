3d04b72b69774901eaabf0e0ce2a5c6506d5bf9c
IvanSvirin <ivan_svirin@mail.ru>
2016-06-14 22:43:14 +0600

    hiding soft input

--- app/src/main/java/com/cashback/ui/MainActivity.java
+++ app/src/main/java/com/cashback/ui/MainActivity.java
@@ -183,6 +183,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                 public void onDrawerSlide(View drawerView, float slideOffset) {
                     InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+                    //
                 }
 
                 @Override
